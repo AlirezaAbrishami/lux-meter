3a2cc1141b8d1cb9f8c8ac8dba59d368988e3549
Alireza Abrishami <alireza.abrishami01@gmail.com>
2020-11-16 17:23:42 +0330

    Initial Commit

---
+++ app/src/main/java/ir/alirezaabrishami/luxmeter/Login.java
@@ -0,0 +1,156 @@
+package ir.alirezaabrishami.luxmeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.PointF;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Login extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener {
+
+    QRCodeReaderView qrCodeReaderView;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+    Button writeCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+
+        if (checkPermission()) {
+            qrCodeReaderView.setOnQRCodeReadListener(Login.this);
+            qrCodeReaderView.setQRDecodingEnabled(true);
+            qrCodeReaderView.setAutofocusInterval(2000L);
+            qrCodeReaderView.setTorchEnabled(true);
+            qrCodeReaderView.setBackCamera();
+        } else {
+            requestPermission();
+        }
+    }
+
+    private void processText(String text) {
+        try {
+            JSONObject jsonObject = new JSONObject(text);
+            String ssid = jsonObject.getString("SSID");
+            String password = jsonObject.getString("Password");
+            connectToAccessPoint(ssid, password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(Login.this, R.string.error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void connectToAccessPoint(String ssid, String password) {
+        try {
+            WifiConfiguration wifiConfig = new WifiConfiguration();
+            wifiConfig.SSID = String.format("\"%s\"", "Alireza iPhone");
+            wifiConfig.preSharedKey = String.format("\"%s\"", "qazwsxedc735");
+
+            WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+            //remember id
+            assert wifiManager != null;
+            int netId = wifiManager.addNetwork(wifiConfig);
+            wifiManager.setWifiEnabled(true);
+            wifiManager.disconnect();
+            wifiManager.enableNetwork(netId, true);
+            wifiManager.reconnect();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    private boolean checkPermission() {
+        // Permission is not granted
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                // main logic
+            } else {
+                Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        showMessageOKCancel("You need to allow access permissions",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestPermission();
+                                    }
+                                }, Login.this);
+                    }
+                }
+            }
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener, Context context) {
+        new AlertDialog.Builder(context)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private void init() {
+        qrCodeReaderView = findViewById(R.id.qr_c);
+        writeCode = findViewById(R.id.login_button);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkPermission()) {
+            qrCodeReaderView.startCamera();
+        } else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        qrCodeReaderView.stopCamera();
+    }
+
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+        assert vibrator != null;
+        vibrator.vibrate(400);
+        processText(text);
+    }
+}
