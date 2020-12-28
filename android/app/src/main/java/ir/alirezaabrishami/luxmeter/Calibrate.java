16687bb5c2f837121dba4f04c4f1326d803558a8
Alireza Abrishami <alireza.abrishami01@gmail.com>
2020-12-28 23:15:36 +0330

    CalibrateActivity Code.

--- android/app/src/main/java/ir/alirezaabrishami/luxmeter/Calibrate.java
+++ android/app/src/main/java/ir/alirezaabrishami/luxmeter/Calibrate.java
@@ -4,13 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Calibrate extends AppCompatActivity {
 
     ImageView nightSwitch;
+    Button calibrateButton;
+    TextView sensorValueText;
+
+    Socket socket;
+    private PrintWriter output;
+    private BufferedReader input;
+    Thread Thread1 = null;
+    final String SERVER_IP = "172.20.10.6";
+    final int SERVER_PORT = 80;
+    boolean sendOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +54,105 @@ public class Calibrate extends AppCompatActivity {
 
             }
         });
+        calibrateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendOk = true;
+            }
+        });
     }
+
+    protected void processMessage(String message) {
+        calibrateButton.setVisibility(View.VISIBLE);
+
+        if (message.charAt(0) == 'o') {
+            Intent intent = new Intent(Calibrate.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            sensorValueText.setText(message);
+        }
+    }
+
+    class Thread1 implements Runnable {
+        public void run() {
+            try {
+                Log.e("CalibrateThread1", "running");
+                socket = new Socket(SERVER_IP, SERVER_PORT);
+                output = new PrintWriter(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                new Thread(new Calibrate.Thread3()).start();
+                new Thread(new Calibrate.Thread2()).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Log.e("CalibrateThread2: ", "running");
+                    while (true) {
+                        Thread.sleep(10);
+                        final String message = input.readLine();
+                        if (message != null) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    processMessage(message);
+                                }
+                            });
+                        } else {
+                            Thread1 = new Thread(new Calibrate.Thread1());
+                            Thread1.start();
+                            return;
+                        }
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class Thread3 implements Runnable {
+        @Override
+        public void run() {
+            output.write("hi");
+            output.flush();
+            Log.e("CalibrateThread3: ", "running");
+            while (true) {
+                if (sendOk) {
+                    output.write("o");
+                    output.flush();
+                    sendOk = false;
+                }
+            }
+        }
+    }
+
 
     private void init() {
         //custom toolbar
         nightSwitch = findViewById(R.id.tool_bar_button);
         Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        calibrateButton = findViewById(R.id.calibrate_button);
+        sensorValueText = findViewById(R.id.calibrate_value);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            if (socket != null)
+                socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
